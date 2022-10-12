21a88f4145879f7ea870de7312a1dcd9a1a763b4
Franklyn Sancho <franklyn.hs@gmail.com>
2022-10-12 12:28:47 -0300

    sistema de troco, adição de parâmetros, controle de preço

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,81 @@
-use std::io;
+use std::{io, collections::btree_map::Values};
 
-struct TotalCoins {
-    value: f64
-}
-
-/* struct Produto {
+struct Produto {
     name: String,
     value: f64,
-} */
+}
 
 
-fn add_coins() {
-    let coins = [0.05, 0.10, 0.25, 0.50, 1.00];
-
-    let mut total_coins = TotalCoins {value: 0.0};
+fn add_coins(mut saldo: f64) {
+    let coins = [0.01, 0.05, 0.10, 0.25, 0.50, 1.00];
 
     loop {
         println!("Insira uma moeda: ");
 
-        let mut coin = String::new();
+        let mut value = String::new();
 
         io::stdin()
-            .read_line(&mut coin)
+            .read_line(&mut value)
             .expect("Falha ao ler entrada");
 
-        let coin: f64 = coin.trim().parse().expect("erro ao inserir moeda");
+        let value: f64 = value.trim().parse().expect("valor inválidos");
 
-        if coins.contains(&coin) {
-            total_coins.value = coin + total_coins.value;
-            println!("Você inseriu {} e seu saldo é {}", coin, total_coins.value);
-        } else if coin == 0.0 {
-            buy_product()
+        if coins.contains(&value) {
+            saldo = value + saldo;
+            println!("Você inseriu {} e seu saldo é {}", value, saldo)
+        } else if value == 0.0 {
+            buy_product(saldo);
         } else {
-            println!("Valor inserido é inválido")
+            println!("Valor inválido")
         }
     }
 }
 
-fn buy_product() {
+fn buy_product(mut saldo: f64) {
 
-    println!("Escolha um produto");
+    
+      let chocolate =  Produto {name: String::from("chocolate"), value: 2.50};
 
-    let mut produto = String::new();
+    /* let chocolate = Produto { name: String::from("chocolate"), value: 2.50}; */
 
-    io::stdin()
-        .read_line(&mut produto)
-        .expect("falha na entrada");
+    loop {
+        println!("Escolha um produto: ");
 
-    println!("Você comprou {produto}")
+        let mut produto = String::new();
 
+        io::stdin()
+            .read_line(&mut produto)
+            .expect("falha na entrada");
 
+        if  produto.contains("chocolate")
+        {
+            saldo = saldo - chocolate.value;
+            println!("Você comprou {} e seu troco é {}: ", chocolate.name, saldo);
+        } else {
+            println!("Produto Inválido")
+        }
+    }
 }
 
 fn main() {
-    add_coins();
-    buy_product();
+    let saldo = 0.0;
+
+
+    println!("Escolha a sua opção: ");
+    println!("1 - inserir moeda");
+    println!("2 - comprar produto");
+
+    let mut option = String::new();
+
+    io::stdin()
+        .read_line(&mut option)
+        .expect("falha na entrada");
+
+    if option.contains("1") {
+        add_coins(saldo)
+    } else if option.contains("2") {
+        buy_product(saldo)
+    } else {
+        println!("Opção inválida!")
+    }
 }
